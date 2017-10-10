1657bf709acde832834253716f94bb921f67e797
mrseifert <31705497+mrseifert@users.noreply.github.com>
2017-10-10 16:54:00 -0400

    Update DeckTest.java

--- PS3/src/test/java/pkgCore/DeckTest.java
+++ PS3/src/test/java/pkgCore/DeckTest.java
@@ -4,29 +4,51 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import pkgEnum.eRank;
+import pkgEnum.eSuit;
+import pkgException.DeckException;
 public class DeckTest {
 
-	@Test
-	public void TestEmptyDeck() {
-		//TODO: Build a deck, draw until you get a DeckException
+	@Test (expected = DeckException.class)
+	public void TestEmptyDeck() throws DeckException{
+		
+		Deck var1 = new Deck();
+		for (int S = 0; S == 53; S++);
+		var1.Draw();
+
 	}
 	
 	@Test
 	public void TestDrawSuit() {
-		//TODO: Build a deck, test the Draw(eSuit) method
+		
+		Deck var2 = new Deck();
+		Card var3 = var2.Draw(eSuit.HEARTS);
+		assertTrue(var3.geteSuit() == eSuit.HEARTS);
+
 	}
 	
 	@Test
 	public void TestDrawRank() {
-		//TODO: Build a deck, test the Draw(eRank) method
+		
+		Deck var4 = new Deck();
+		Card var5 = var4.Draw(eRank.JACK);
+		assertTrue(var5.geteRank() == eRank.JACK);
+
 	}
 	
 	public void TestDeckRankCount() {
-		//TODO: Build a deck, test the DeckRankCount method
+		
+		Deck var6 = new Deck();
+		assertTrue(var6.Count(eRank.TEN) == 4);
+		
+
 	}
 	
 	public void TestDeckSuitCount() {
-		//TODO: Build a deck, test the DeckSuitCount method
+		
+		Deck var7 = new Deck();
+		assertTrue(var7.Count(eSuit.SPADES) == 13);
+
 	}
 
 }
